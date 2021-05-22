7a436f6063047144172831d8e893960acdda2548
vs462 <60940617+vs462@users.noreply.github.com>
2021-05-22 14:35:29 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+info =[{'subdomain' : 'test_sub',
+        'dataset_gp' : 11,
+        'dataset_ap' : 12,
+        'external_id' : 'test.io',
+        'from_date' : '2021-01-01',
+        'to_date' : '2021-02-01'},
+        {'subdomain' : 'test_sub2',
+        'dataset_gp' : 113,
+        'dataset_ap' : 132,
+        'external_id' : 'test.io2',
+        'from_date' : '2021-01-01',
+        'to_date' : '2021-02-01'},
+        {'subdomain' : 'test_su222b',
+        'dataset_gp' : 1121,
+        'dataset_ap' : 123,
+        'external_id' : 'test.io',
+        'from_date' : '2021-01-01',
+        'to_date' : '2021-02-01'}]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/app1")
+def app1():
+    return render_template('app1.html', info=info)
+
+if __name__=='__main__':
+    app.run(debug=True)
