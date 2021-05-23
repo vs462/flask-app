f1223613a26013508ec947b01db03eed278d3203
vs462 <60940617+vs462@users.noreply.github.com>
2021-05-23 20:47:13 +0100

    update styles

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
 
 info =[{'subdomain' : 'test_sub',
         'dataset_gp' : 11,
